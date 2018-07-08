283e8c07fc79fcdbf6f76e0e945be25d62ccdef1
Henryk <jatranscedentalne@gmail.com>
2018-07-08 09:01:48 +0200

    5.rb

--- drugi_zjazd/5.rb
+++ drugi_zjazd/5.rb
@@ -8,6 +8,10 @@ class Book
     @title
   end
 
+def title=(title)
+  @title = title
+end
+
   def number_of_pages
     @number_of_pages
   end
@@ -23,9 +27,29 @@ books = [
   Book.new("W pustyni i w puszczy", 300)
 ]
 
-def book_exists?(books, title)
-  books.any? {|e| e.title == title}
+# book = books[0]
+# puts book
+# book.title = "Inny tytuł"
+# puts book
+
+def add_additional_info_to_books(books)
+  books.length.times do |i|
+        if books[i].number_of_pages > 200
+           books[i].title += "(gruba książka)"
+        end
+    end
+    return books
 end
 
-puts book_exists?(books, "Sierotka Marysia")
-puts book_exists?(books, "Pan Tadeusz")
+books_with_additional_info = add_additional_info_to_books(books)
+puts books_with_additional_info
+
+#jeśli książka ma więcej niż 200 stron, to dać dodatkową informację przy tytule
+#w nawiasie
+
+# def book_exists?(books, title)
+#   books.any? {|e| e.title == title}
+# end
+#
+# puts book_exists?(books, "Sierotka Marysia")
+# puts book_exists?(books, "Pan Tadeusz")
